2a33f5289be54f8b2c3313bae9155a0d435ecd50
unknown <kulangana@yandex.ru git config --global user.name Kulangana-ddgit --global user.email kulangana@yandex.ru>
2023-01-09 00:06:09 +0300

    metaclass

---
+++ metaclass.py
@@ -0,0 +1,17 @@
+class MyMetaClass(type):
+    _instance = None
+
+    def __call__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__call__(*args, **kwargs)
+        return cls._instance 
+
+
+class MyTest(metaclass=MyMetaClass):
+    pass
+
+
+a = MyTest()
+b = MyTest()
+c = MyTest()
+print('a is b is c —', a is b is c)
